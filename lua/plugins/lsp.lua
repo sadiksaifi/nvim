bb2a191ec7a5d4e751caddf1e45b59fd435cefd7
Sadik Saifi <Sadiksaifi205@gmail.com>
2023-04-13 02:51:08 +0530

    fix: add lua_ls config for disabling vim not difined error

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -75,6 +75,27 @@ return {
       omit = { "<Tab>", "S<Tab>" },
     })
 
+
+    lsp.configure('lua_ls', {
+      capabilities = capabilities,
+      on_attach = on_attach,
+      settings = { -- custom settings for lua
+        Lua = {
+          -- make the language server recognize "vim" global
+          diagnostics = {
+            globals = { "vim" },
+          },
+          workspace = {
+            -- make language server aware of runtime files
+            library = {
+              [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+              [vim.fn.stdpath("config") .. "/lua"] = true,
+            },
+          },
+        },
+      },
+    })
+
     lsp.setup()
 
     -- Snippets
