a09bb4382edbdbd46233d412638ca424918c2b7a
Sadik Saifi <Sadiksaifi205@gmail.com>
2023-04-12 23:10:50 +0530

    feat: add usefull autocmd

--- lua/core/autocommands.lua
+++ lua/core/autocommands.lua
@@ -1,6 +1,32 @@
-vim.api.nvim_create_autocmd('TextYankPost', {
-  callback = function() 
-      vim.highlight.on_yank() 
-    end,
-  desc = "Briefly highlight yanked text"
+vim.api.nvim_create_autocmd("TextYankPost", {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  desc = "Briefly highlight yanked text",
+})
+
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "qf", "help", "man", "lspinfo", "spectre_panel" },
+  callback = function()
+    vim.cmd([[
+      nnoremap <silent> <buffer> q :close<CR>
+      set nobuflisted
+    ]])
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "gitcommit", "markdown" },
+  callback = function()
+    vim.opt_local.wrap = true
+    vim.opt_local.spell = true
+  end,
+})
+-- Automatically close tab/vim when nvim-tree is the last window in the tab
+vim.cmd("autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif")
+
+vim.api.nvim_create_autocmd({ "VimResized" }, {
+  callback = function()
+    vim.cmd("tabdo wincmd =")
+  end,
 })
