8ada445facef215ca4637b4ab2a710fea575040f
Sadik Saifi <Sadiksaifi205@gmail.com>
2023-04-13 18:21:32 +0530

    feat: add project manager

---
+++ lua/plugins/project.lua
@@ -0,0 +1,24 @@
+return {
+  "ahmedkhalf/project.nvim",
+  config = function()
+    local status_ok, project = pcall(require, "project_nvim")
+    if not status_ok then
+      return
+    end
+    project.setup {
+
+      -- detection_methods = { "lsp", "pattern" }, -- NOTE: lsp detection will get annoying with multiple langs in one project
+      detection_methods = { "pattern" },
+
+      -- patterns used to detect root dir, when **"pattern"** is in detection_methods
+      patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json" },
+    }
+
+    local tele_status_ok, telescope = pcall(require, "telescope")
+    if not tele_status_ok then
+      return
+    end
+
+    telescope.load_extension "projects"
+  end
+}
