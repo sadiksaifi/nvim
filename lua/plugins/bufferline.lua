5b2b94bec74ac7e98c7e4e177715e52311da3ea3
Sadik Saifi <Sadiksaifi205@gmail.com>
2023-04-15 17:31:30 +0530

    feat: add buff delete

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,20 +1,25 @@
 return {
-  'akinsho/bufferline.nvim', 
-  dependencies = 'nvim-tree/nvim-web-devicons',
-  config = function ()
-    -- Using protected call
-    local status_ok, bufferline = pcall(require, "bufferline")
-    if not status_ok then
-      return
-    end
+	"akinsho/bufferline.nvim",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+		"famiu/bufdelete.nvim",
+	},
+	config = function()
+		-- Using protected call
+		local status_ok, bufferline = pcall(require, "bufferline")
+		if not status_ok then
+			return
+		end
 
-    bufferline.setup {
-      options = {
-        close_command = "Bdelete! %d", -- can be a string | function, see "Mouse actions"
-        right_mouse_command = "Bdelete! %d", -- can be a string | function, see "Mouse actions"
-        offsets = { { filetype = "NvimTree", text = "", padding = 1 } },
-        separator_style = "thin", -- | "thick" | "thin" | { 'any', 'any' },
-      }
-    }
-  end
+		bufferline.setup({
+			options = {
+				close_command = "Bdelete! %d", -- can be a string | function, see "Mouse actions"
+				right_mouse_command = "Bdelete! %d", -- can be a string | function, see "Mouse actions"
+				offsets = { { filetype = "NvimTree", text = "", padding = 1 } },
+				separator_style = "thin", -- | "thick" | "thin" | { 'any', 'any' },
+			},
+		})
+
+    vim.keymap.set("n", "<leader>x", ":Bdelete<CR>", { noremap = true, silent = true })
+	end,
 }
