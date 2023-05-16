55df5708c4f268c8e95b40e478ae3c0010920cf9
Sadik Saifi <Sadiksaifi205@gmail.com>
2023-05-16 12:36:33 +0530

    feat: add markdown-preview keymap.

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -3,9 +3,13 @@ return {
 	build = function()
 		vim.fn["mkdp#util#install"]()
 	end,
-  config = function ()
-    vim.g.mkdp_theme = 'light'
-    vim.g.mkdp_auto_start = 0
-    vim.g.mkdp_auto_close = 1
-  end,
+  ft = { "markdown", "md"},
+	keys = {
+		{ "<leader>mp", "<cmd>MarkdownPreview<CR>", desc = "Markdown Preview" },
+	},
+	config = function()
+		vim.g.mkdp_theme = "light"
+		vim.g.mkdp_auto_start = 0
+		vim.g.mkdp_auto_close = 1
+	end,
 }
