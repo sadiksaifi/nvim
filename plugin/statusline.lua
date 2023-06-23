20b8b1ba8dd8388d407b7c08f98690051ca1fab4
Sadik Saifi <Sadiksaifi205@gmail.com>
2023-06-23 19:51:51 +0530

    fix: the staustline

--- plugin/statusline.lua
+++ plugin/statusline.lua
@@ -13,17 +13,17 @@ let g:currentmode={
 
 function! GetGitBranch()
   let branch = system('git rev-parse --abbrev-ref HEAD 2>/dev/null')
-  return empty(branch) ? '' : ' ' . branch[:-2]
+  return empty(branch) ? '' : '   ' . branch[:-2] . ' '
 endfunction
 
 set statusline= " Clear statusline
 set statusline+=\%#RedrawDebugComposed# " Set highlight group to CursorLine 
 set statusline+=\ %{toupper(g:currentmode[mode()])} " Show current mode
 set statusline+=\%#NeogitDiffAddHighlight# " Set highlight group to CursorLine 
-set statusline+=\ %{GetGitBranch()} " Show Git branch
-set statusline+=\ %#CursorLine# " Set highlight group to CursorLine 
+set statusline+=\%{GetGitBranch()} " Show Git branch
+set statusline+=\%#CursorLine# " Set highlight group to CursorLine 
 set statusline+=%= " Right align following items
-set statusline+=\%{&modified?'[+]':''} " Show [+} if file is modified
+set statusline+=\%{&modified?'[+]':''} " Show [+] if file is modified
 set statusline+=\%r " Shows status of read only files
 set statusline+=\ %f " Show path to file
 set statusline+=\ %#NeogitDiffAddHighlight# " Set highlight group to CursorLine 
