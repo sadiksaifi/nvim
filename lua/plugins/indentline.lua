66774ef9f7e27a0eb02c6038f03554143ed31fe0
Sadik Saifi <thesadiksaifi@gmail.com>
2023-11-15 15:33:09 +0530

    chore: update ibl latest version

--- lua/plugins/indentline.lua
+++ lua/plugins/indentline.lua
@@ -1,17 +1,5 @@
 return {
-	"lukas-reineke/indent-blankline.nvim",
-  event = "BufReadPre",
-	opts = {
-		char = "▏",
-		show_trailing_blankline_indent = false,
-		show_first_indent_level = true,
-		use_treesitter = true,
-		show_current_context = true,
-		buftype_exclude = { "terminal", "nofile" },
-		filetype_exclude = {
-			"help",
-			"packer",
-			"NvimTree",
-		},
-	},
+  "lukas-reineke/indent-blankline.nvim",
+  main = "ibl",
+  opts = {},
 }
