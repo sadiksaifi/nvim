b6b53a5410a5144fbe8f80ac1f9d179f88562d41
Sadik Saifi <thesadiksaifi@gmail.com>
2024-01-10 15:07:30 +0530

    chore: rollback autocmd for LspFormat

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -31,24 +31,24 @@ return {
           sources = {
             formatting.prettier,
           },
-          on_attach = function(current_client, bufnr)
-            if current_client.supports_method("textDocument/formatting") then
-              vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-              vim.api.nvim_create_autocmd("BufWritePre", {
-                group = augroup,
-                buffer = bufnr,
-                callback = function()
-                  vim.lsp.buf.format({
-                    filter = function(client)
-                      --  only use null-ls for formatting instead of lsp server
-                      return client.name == "null-ls"
-                    end,
-                    bufnr = bufnr,
-                  })
-                end,
-              })
-            end
-          end,
+--          on_attach = function(current_client, bufnr)
+--            if current_client.supports_method("textDocument/formatting") then
+--              vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+--              vim.api.nvim_create_autocmd("BufWritePre", {
+--                group = augroup,
+--                buffer = bufnr,
+--                callback = function()
+--                  vim.lsp.buf.format({
+--                    filter = function(client)
+--                      --  only use null-ls for formatting instead of lsp server
+--                      return client.name == "null-ls"
+--                    end,
+--                    bufnr = bufnr,
+--                  })
+--                end,
+--              })
+--            end
+--          end,
         })
       end,
     },
