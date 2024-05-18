01351029c10882930210777ee5b0ffd26dc3cc1d
Sadik Saifi <email@sadiksaifi.dev>
2024-05-18 20:46:15 +0530

    chore: add code theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,30 +1,12 @@
--- return {
---   "askfiy/visual_studio_code",
---   priority = 100,
---   config = function()
---     require("visual_studio_code").setup({
---       transparent = true,
---     })
---
---     vim.cmd("colorscheme visual_studio_code")
---   end,
--- }
 return {
-  "ellisonleao/gruvbox.nvim",
-  priority = 1000,
+  "askfiy/visual_studio_code",
+  priority = 100,
   config = function()
-    require("gruvbox").setup({
-      transparent_mode = true,
-        italic = {
-          strings = false,
-          emphasis = true,
-          comments = true,
-          operators = false,
-          folds = true,
-        },
-    })
+    local code = require "visual_studio_code"
+    code.setup {
+      transparent = true,
+    }
 
-    vim.cmd("colorscheme gruvbox")
+    vim.cmd "colorscheme visual_studio_code"
   end,
--- #1c2021
 }
