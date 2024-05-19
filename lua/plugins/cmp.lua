e60b2e60a2d2f79b41a05de39372a482d27e399f
Sadik Saifi <email@sadiksaifi.dev>
2024-05-20 00:05:24 +0530

    chore: remove crap

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -9,7 +9,6 @@ return {
   config = function()
     local cmp = require "cmp"
     local luasnip = require "luasnip"
-    local icons = require "user.icons"
 
     cmp.setup {
       snippet = {
@@ -31,36 +30,6 @@ return {
         { name = "nvim_lsp" },
         { name = "luasnip" },
         { name = "buffer" },
-      },
-      experimental = {
-        ghost_text = false,
-        native_menu = false,
-      },
-      ---@diagnostic disable-next-line: missing-fields
-      formatting = {
-        fields = { "abbr", "menu", "kind" },
-        format = function(entry, vim_item)
-          vim_item.kind = icons.kind[vim_item.kind]
-          vim_item.menu = ({
-            nvim_lsp = "",
-            nvim_lua = "",
-            luasnip = "",
-            buffer = "",
-            path = "",
-            emoji = "",
-          })[entry.source.name]
-
-          return vim_item
-        end,
-      },
-      window = {
-        completion = {
-          border = "rounded",
-          scrollbar = false,
-        },
-        documentation = {
-          border = "rounded",
-        },
       },
     }
   end,
--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -4,41 +4,13 @@ return {
   dependencies = "folke/neodev.nvim",
   config = function()
     local lspconfig = require "lspconfig"
-    local icons = require "user.icons"
 
     -- diagnostic config
     vim.diagnostic.config({
-      signs = {
-        active = true,
-        values = {
-          { name = "DiagnosticSignError", text = icons.diagnostics.Error },
-          { name = "DiagnosticSignWarn", text = icons.diagnostics.Warning },
-          { name = "DiagnosticSignHint", text = icons.diagnostics.Hint },
-          { name = "DiagnosticSignInfo", text = icons.diagnostics.Information },
-        },
-      },
       virtual_text = true,
       update_in_insert = false,
       underline = true,
-      severity_sort = true,
-      float = {
-        focusable = true,
-        style = "minimal",
-        border = "rounded",
-        source = "always",
-        header = "",
-        prefix = "",
-      },
     })
-
-    for _, sign in ipairs(vim.tbl_get(vim.diagnostic.config(), "signs", "values") or {}) do
-      vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = sign.name })
-    end
-
-    vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
-    vim.lsp.handlers["textDocument/signatureHelp"] =
-    vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" })
-    require("lspconfig.ui.windows").default_options.border = "rounded"
 
     local on_attach = function(client, bufnr)
       local opts = { noremap = true, silent = true, buffer = bufnr }
