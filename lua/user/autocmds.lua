c904e689d85b4ca3d1ad0da0fa1487a32f257fbc
Sadik Saifi <email@sadiksaifi.dev>
2024-06-28 18:20:05 +0530

    lollllllllllllo

--- lua/user/autocmds.lua
+++ lua/user/autocmds.lua
@@ -5,14 +5,6 @@ vim.api.nvim_create_autocmd("TextYankPost", {
   desc = "Briefly highlight yanked text",
 })
 
-vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = { "gitcommit", "markdown" },
-  callback = function()
-    vim.opt_local.wrap = true
-    vim.opt_local.spell = true
-  end,
-})
-
 vim.api.nvim_create_autocmd({ "VimResized" }, {
   callback = function()
     vim.cmd("tabdo wincmd =")
--- plugin/statusline.lua
+++
@@ -1,1 +0,0 @@
-vim.cmd("hi StatusLine guibg=NONE")
