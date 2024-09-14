f941f733b1aec3206c73214eb1663425e83ff4aa
Sadik Saifi <email@sadiksaifi.dev>
2024-09-15 01:35:10 +0530

    feat: add copilot chat

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,46 +1,73 @@
 return {
-  "zbirenbaum/copilot.lua",
-  cmd = "Copilot",
-  event = "InsertEnter",
-  keys = {
-    { "<leader>tc", "<cmd>Copilot toggle<CR>", desc = "Copilot toggle" },
-  },
-  opts = {
-    panel = {
-      enabled = true,
-      auto_refresh = false,
-      keymap = {
-        jump_prev = "[[",
-        jump_next = "]]",
-        accept = "<CR>",
-        refresh = "gr",
-        open = "<M-CR>",
-      },
-      layout = {
-        position = "bottom", -- | top | left | right
-        ratio = 0.4,
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    branch = "canary",
+    dependencies = {
+      {
+        "zbirenbaum/copilot.lua",
+        cmd = "Copilot",
+        event = "InsertEnter",
+        keys = {
+          { "<leader>ct", "<cmd>Copilot toggle<CR>", desc = "Copilot toggle" },
+        },
+        opts = {
+          panel = {
+            enabled = true,
+            auto_refresh = false,
+            keymap = {
+              jump_prev = "[[",
+              jump_next = "]]",
+              accept = "<CR>",
+              refresh = "gr",
+              open = "<M-CR>",
+            },
+            layout = {
+              position = "bottom", -- | top | left | right
+              ratio = 0.4,
+            },
+          },
+          suggestion = {
+            enabled = true,
+            auto_trigger = true,
+            debounce = 50,
+            keymap = {
+              accept = "<M-l>",
+              dismiss = "<M-h>",
+              accept_word = false,
+              accept_line = false,
+              next = "<M-]>",
+              prev = "<M-[>",
+            },
+          },
+          filetypes = {
+            ["."] = true,
+            go = false,
+            c = false,
+            rust = false,
+            cpp = false,
+          },
+          server_opts_overrides = {},
+        },
       },
     },
-    suggestion = {
-      enabled = true,
-      auto_trigger = true,
-      debounce = 75,
-      keymap = {
-        accept = "<M-l>",
-        dismiss = "<M-h>",
-        accept_word = false,
-        accept_line = false,
-        next = "<M-]>",
-        prev = "<M-[>",
+    build = "make tiktoken", -- Only on MacOS or Linux
+    keys = {
+      {
+        "<leader>cp",
+        function()
+          local actions = require "CopilotChat.actions"
+          require("CopilotChat.integrations.telescope").pick(actions.prompt_actions())
+        end,
+        desc = "CopilotChat - Prompt actions",
+      },
+      {
+        "<leader>cc",
+        "<cmd>CopilotChat<CR>",
+        desc = "CopilotChat - Quick chat",
       },
     },
-    filetypes = {
-      ["."] = true,
-      go = false,
-      c = false,
-      rust = false,
-      cpp = false,
+    opts = {
+      debug = false,
     },
-    server_opts_overrides = {},
   },
 }
