eb89d15052ccee43bfad111dabeb48fdd4941201
Sadik Saifi <email@sadiksaifi.dev>
2025-02-06 17:56:42 +0530

    chore: default clrscheme

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,13 +1,27 @@
 return {
-  "supermaven-inc/supermaven-nvim",
-  config = function()
-    require("supermaven-nvim").setup {
-      keymaps = {
-        accept_suggestion = "<C-l>",
-        clear_suggestion = "<C-h>",
-        accept_word = "<C-j>",
+  {
+    "supermaven-inc/supermaven-nvim",
+    config = function()
+      require("supermaven-nvim").setup {
+        keymaps = {
+          accept_suggestion = "<C-l>",
+          clear_suggestion = "<C-h>",
+          accept_word = "<C-j>",
+        },
+        -- ignore_filetypes = { cpp = true }, -- or { "cpp", }
       }
-      -- ignore_filetypes = { cpp = true }, -- or { "cpp", }
-    }
-  end,
+    end,
+  },
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    dependencies = {
+      { "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
+      { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+    },
+    build = "make tiktoken", -- Only on MacOS or Linux
+    opts = {
+      -- See Configuration section for options
+    },
+    -- See Commands section for default commands if you want to lazy load on them
+  },
 }
---
+++ plugin/colorscheme.lua
@@ -0,0 +1,2 @@
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
