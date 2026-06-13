aeafdf23c054763fb49a0e44308126246c70e317
Sadik Saifi <mail@sadiksaifi.dev>
2026-06-13 20:38:00 +0530

    feat(neogit): enable neogit ui

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -1,18 +1,17 @@
-return {}
--- return {
---   "NeogitOrg/neogit",
---   lazy = true,
---   dependencies = {
---     "nvim-lua/plenary.nvim", -- required
---
---     -- Only one of these is needed.
---     "sindrets/diffview.nvim", -- optional
---
---     -- Only one of these is needed.
---     "folke/snacks.nvim", -- optional
---   },
---   cmd = "Neogit",
---   keys = {
---     { "<leader>G", "<cmd>Neogit<cr>", desc = "Show Neogit UI" },
---   },
--- }
+return {
+  "NeogitOrg/neogit",
+  lazy = true,
+  dependencies = {
+    "nvim-lua/plenary.nvim", -- required
+
+    -- Only one of these is needed.
+    "sindrets/diffview.nvim", -- optional
+
+    -- Only one of these is needed.
+    "folke/snacks.nvim", -- optional
+  },
+  cmd = "Neogit",
+  keys = {
+    { "<leader>G", "<cmd>Neogit<cr>", desc = "Show Neogit UI" },
+  },
+}
